e3fb9d3b9a24bc91180c107be2ffe4ce3f099f19
LankaActionFoundation <lankaactionfoundation@gmail.com>
2022-04-30 13:52:10 +0530

    optiomole added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,34 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "../styles/froala.css";
-
+import Head from "next/head";
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+  (function(w, d){
+    var b = d.getElementsByTagName('head')[0];
+    var s = d.createElement("script");
+    var v = ("IntersectionObserver" in w) ? "_no_poly" : "";
+    s.async = true; // This includes the script as async.
+    s.src = "https://d5jmkjjpb7yfg.cloudfront.net/v2/latest/optimole_lib" + v + ".min.js";
+    w.optimoleData = {
+        key: "mllfvjtztoh1",
+        quality: "85",
+    };
+    b.appendChild(s);
+}(window, document));
+  `,
+        }}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
