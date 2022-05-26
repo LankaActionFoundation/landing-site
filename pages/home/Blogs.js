a6c3309be119b5d4af42c32f60ad58ba38f1e07f
LankaActionFoundation <94227017+LankaActionFoundation@users.noreply.github.com>
2022-05-26 15:53:39 +0530

    home page changes

--- pages/home/Blogs.js
+++ pages/home/Blogs.js
@@ -1,56 +1,48 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
-import FilledButton from '../../components/buttons/FilledButton';
-import BlogCard from '../../components/BlogCard';
+import FilledButton from "../../components/buttons/FilledButton";
+import BlogCard from "../../components/BlogCard";
 
-const Blogs = ({blogs}) => {
-    return(<>
-        <section className="bg-white w-full">
-            <div className="w-full py-20 max-w-6xl mx-auto px-3 lg:px-0">
-              
-              <div className="">
-                {blogs && blogs.length > 0 && (
-                  <>
-                    <div className="w-full flex flex-col md:flex-row items-start justify-between">
-                      <h2 className="text-3xl md:text-6xl font-title text-gray-800 font-bold">
-                        Latest Articles
-                      </h2>
+const Blogs = ({ blogs }) => {
+  return (
+    <>
+      <section className="bg-white w-full">
+        <div className="w-full py-20 max-w-6xl mx-auto px-3 lg:px-0">
+          <div className="">
+            {blogs && blogs.length > 0 && (
+              <>
+                <div className="heading-2">
+                  <h2 className="mt-5 text-3xl md:text-5xl font-title text-gray-800  sm:w-auto  mx-auto block text-left font-bold pull-left">
+                    Latest Articles
+                  </h2>
+                  <div className="viewAll">
+                    <Link href={"/blog/1"}>View All</Link>
+                  </div>
+                  <div className="clear-both"></div>
+                </div>
 
-                      <div className="md:mt-5 w-full md:w-auto">
-                        <Link href="/blog/1">
-                          <FilledButton className="w-full" color="green">
-                            <span className="px-5 text-white uppercase whitespace-nowrap">
-                              All Articles
-                            </span>
-                          </FilledButton>
-                        </Link>
-                      </div>
+                <div className="mt-10 w-full lg:w-auto flex flex-col md:flex-row items-center justify-center gap-4">
+                  {blogs.map((blog) => (
+                    <div className="w-full lg:max-w-md">
+                      <BlogCard
+                        widthFull
+                        slug={blog.slug}
+                        title={blog.title}
+                        subTitle={blog.subtitle}
+                        thumbnail={blog.thumbnail}
+                        category={blog.category}
+                        date={blog.updated_at}
+                      />
                     </div>
-
-                    <div className="mt-10 w-full lg:w-auto flex flex-col md:flex-row items-center justify-center gap-4">
-                      {blogs.map((blog) => (
-                        <div className="w-full lg:max-w-md">
-                          <BlogCard
-                            widthFull
-                            slug={blog.slug}
-                            title={blog.title}
-                            subTitle={blog.subtitle}
-                            thumbnail={blog.thumbnail}
-                            category={blog.category}
-                            date={blog.updated_at}
-                          />
-                        </div>
-                      ))}
-
-
-                    </div>
-                  </>
-                )}
-                
-              </div>
-            </div>
-          </section>
-    </>);
-}
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Blogs;
--- pages/home/Projects.js
+++ pages/home/Projects.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, {useEffect, useState} from 'react';
 import CircularDonationCard from "../../components/CircularDonationCard";
 
@@ -5,17 +6,25 @@ const Projects = ({donations}) => {
 
     return(<>
     <section className="bg-white w-full">
-            <div className="w-full max-w-6xl mx-auto py-10 px-3 lg:px-0">
+            <div className="w-full max-w-7xl mx-auto py-10 px-3 lg:px-0">
               {/* <h5 className="w-full block text-center text-base md:text-lg text-gray-800">
               Give a hand to make
             </h5> */}
-
-              <h2 className="mt-5 text-3xl md:text-4xl font-title text-gray-800 w-full max-w-lg xl:max-w-none mx-auto block text-center font-bold">
-                The smallest donation can change someone’s life. <br />
+            <div className='heading-2'>
+              <h2 className="mt-5 text-3xl md:text-5xl font-title text-gray-800  sm:w-auto  mx-auto block text-left font-bold pull-left">
+                The smallest donation can change someone’s life.
               </h2>
+              <div className='viewAll'><Link href={'/donation/upcoming-projects/1'}>View All</Link></div>
+              <div className='clear-both'></div>
+            </div>
+
+              {/* <h2 className="mt-5 text-3xl md:text-5xl font-title text-gray-800 w-3/4 sm:w-auto  mx-auto block text-left font-bold heading-2">
+                The smallest donation can change someone’s life. <br />
+              </h2> */}
 
               {donations && donations.length > 0 && (
-                <div className="mt-10 w-full md:w-auto flex flex-col md:flex-row items-baseline justify-center gap-14">
+                // <div className="mt-10 w-full md:w-auto flex flex-col md:flex-row items-baseline justify-center gap-4">
+                <div className="mt-10 w-full md:w-auto grid grid-cols-1 md:grid-cols-3 justify-center gap-4">
                   {donations.map((donation) => (
                     <CircularDonationCard
                       slug={donation.slug}
@@ -23,6 +32,7 @@ const Projects = ({donations}) => {
                       thumbnail={donation.thumbnail}
                       raised={parseInt(donation.reached)}
                       goal={parseInt(donation.goal)}
+                      subtitle={donation.subtitle}
                     />
                   ))}
                 </div>
